f34489e9bd711032bd48cca8168700312cf1cf2b
Ji Hyeuk Park <jhapark21@gmail.com>
2018-11-08 12:13:33 -0800

    ExerciseAbstract

---
+++ The Tech Academy Basic C-Sharp Projects/ExerciseAbstract/ExerciseAbstract/Employee.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseAbstract
+{
+    class Employee : Person
+    {
+        
+        public Employee(string FirstName, string LastName) : base(FirstName, LastName)
+        {
+
+        }
+        public override void SayName()
+        {
+            Console.WriteLine(base.FirstName + " " + base.LastName);
+        }
+
+    }
+}
---
+++ The Tech Academy Basic C-Sharp Projects/ExerciseAbstract/ExerciseAbstract/Program.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseAbstract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee("Sample", "Student");
+            employee.SayName();
+
+            Console.ReadLine();
+
+        }
+    }
+}
