6ce626be0a416c2220e97ff5a6198162be108f4a
Ji Hyeuk Park <jhapark21@gmail.com>
2018-11-07 16:06:48 -0800

    ExerciseSuperclass

---
+++ The Tech Academy Basic C-Sharp Projects/ExcerciseSuperclass/ExcerciseSuperclass/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcerciseSuperclass
+{
+    class Employee : Person
+    {
+        int Id = 0;
+
+        public Employee(string FirstName, string LastName) : base(FirstName, LastName)
+        {
+
+        }
+
+
+    }
+}
---
+++ The Tech Academy Basic C-Sharp Projects/ExcerciseSuperclass/ExcerciseSuperclass/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcerciseSuperclass
+{
+    class Person
+    {
+
+        string FirstName;
+        string LastName;
+
+        public Person(string FirstName, string LastName)
+        {
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+        }
+
+        public void SayName()
+        {
+            Console.WriteLine("Name: [" + FirstName + " " + LastName + "]");
+        }
+        
+    }
+}
