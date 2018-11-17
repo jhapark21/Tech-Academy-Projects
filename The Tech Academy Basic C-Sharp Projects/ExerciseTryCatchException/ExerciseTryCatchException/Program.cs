07e00f64b128883ae1e69d37f94d1a073b8b1f14
Ji Hyeuk Park <jhapark21@gmail.com>
2018-11-16 16:23:24 -0800

    ExerciseTryCatchException

--- The Tech Academy Basic C-Sharp Projects/ExerciseTryCatchException/ExerciseTryCatchException/Program.cs
+++ The Tech Academy Basic C-Sharp Projects/ExerciseTryCatchException/ExerciseTryCatchException/Program.cs
@@ -10,34 +10,32 @@ namespace ExerciseTryCatchException
     {
         static void Main(string[] args)
         {
-            Console.Write("How old are you?: ");
+            Start:
             try
             {
                 bool validAnswer = false;
                 int age = 0;
                 while (!validAnswer)
                 {
+                    Console.Write("How old are you?: ");
                     validAnswer = int.TryParse(Console.ReadLine(), out age);
                     if (!validAnswer) Console.WriteLine("Please enter integers only.");
                 }
-                if (age <0)
+                if (age <= 0)
                 {
                     throw new AgeException();
                 }
-
                 Console.WriteLine("You are born in " + DateTime.Now.AddYears(-age).Year);
             }
             catch (AgeException)
             {
-                Console.WriteLine("Your age cannot be a negative.");
-                Console.ReadLine();
-                return;
+                Console.WriteLine("Your age cannot be zero or negative.");
+                goto Start;
             }
             catch (Exception)
             {
                 Console.WriteLine("An error occured. Please contact your System Administrator.");
-                Console.ReadLine();
-                return;
+                goto Start;
             }
 
 
